7b4649de43e7b248553b7619e0ff293e15e89d65
samlewin19 <144487001+samlewin19@users.noreply.github.com>
2023-09-08 15:19:38 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+  public static void main(String[] args)
+  {
+    System.out.print("Hello, World!");
+    System.out.println("Hi, Computer!");
+    System.out.println("Use println so we're not on the same line");
+  }
+}
