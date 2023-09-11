112eda18666512770af6e1f83f3cb46047649fe7
samlewin19 <144487001+samlewin19@users.noreply.github.com>
2023-09-11 15:10:30 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,8 +2,12 @@ public class Main
 {
   public static void main(String[] args)
   {
-    System.out.print("Hello, World!");
-    System.out.println("Hi, Computer!");
-    System.out.println("Use println so we're not on the same line");
+    final int x = 5;
+    final boolean y = false;
+    final double z = 5.5;
+    System.out.println(x);  
+    System.out.println(y);  
+    System.out.println(z);
   }
+  
 }
