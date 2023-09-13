6caa11444aff7990ff86aa4203b4caa54a6067ce
samlewin19 <144487001+samlewin19@users.noreply.github.com>
2023-09-13 15:24:59 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Main
 {
   public static void main(String[] args)
@@ -10,12 +11,25 @@ public class Main
     System.out.println(z);
     double xz = x + z;
     System.out.println(xz + z * x);
+    Scanner s = new Scanner(System.in);
+    System.out.println("What's your favorite number?");
+    int favNum = s.nextInt();
+    System.out.println("Now let's try and divide your favorite number by 0");
     try {
-    System.out.println(x / 0);
+    System.out.println(favNum / 0);
         }
     catch (Exception e) {
-    System.out.println("You can't divide an integer by 0");
-                         }
+    System.out.println(e + "\nYou can't divide an integer by 0");
+    }
+    System.out.println("Pick a new number to divide by.");
+    double newNum = s.nextDouble();
+    System.out.println(favNum / newNum);
+    System.out.println("\nCool beans dude! \nOh, I never caught your name. What's your name?");
+    s = new Scanner(System.in);
+    String name = s.nextLine();
+    System.out.println("Hi, " + name + "!");
+    s.close();
+                         
     
   }
 }
