917f4ae94056d083363a7cc68989b7c27f745969
samlewin19 <144487001+samlewin19@users.noreply.github.com>
2023-09-15 15:30:45 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,45 +1,52 @@
-package Main;
-
 import java.util.Scanner;
-
-public class Main 
+public class Main
 {
-	public static void main(String[] args)
-	{
-		Scanner s = new Scanner(System.in);
-		System.out.println("What is your name?");
-		String name = s.nextLine();
-		System.out.println("Hey " + name + ", what did you get on the test?");
-		double yourScore = s.nextDouble(); //prevents the variable (yourScore) from being changed. 
-		
-		System.out.println("Really? You got a " + yourScore + ".");
-		int averageScore = 88; //Uses double bc the numeric value is a decimal
-		System.out.println("The average score is " + averageScore + ".");
-		System.out.println("Did you do the extra credit assignemnt?");
-		String extraCredit = s.nextLine();
-		
-		double averageScoreUpdated = (averageScore + yourScore) / 2;
-		
-		if (extraCredit .equals("yes") ) {
-			yourScore += 5;
-		}
-		
-		System.out.println("Did you do above average?");
-		
-		if (yourScore > averageScoreUpdated) {
-			boolean aboveAverage = true;
-			System.out.print(aboveAverage);
-		} else {
-			if (yourScore < averageScoreUpdated) {
-			boolean aboveAverage = false;
-			System.out.print(aboveAverage);
-		} else {
-			if (yourScore == averageScoreUpdated) {
-				System.out.print("You did average!");
-			}
-		}
-		}
-		
-		
-	}
+  public static void main(String[] args)
+  {
+    final int x = 5;
+    final boolean y = false;
+    final double z = 5.5;
+    System.out.println(x);  
+    System.out.println(y);  
+    System.out.println(z);
+    double xz = x + z;
+    System.out.println(xz + z * x);
+    Scanner s = new Scanner(System.in);
+    System.out.println("What's your favorite number?");
+    int favNum = s.nextInt();
+    System.out.println("Now let's try and divide your favorite number by 0");
+    try {
+    System.out.println(favNum / 0);
+        }
+    catch (Exception e) {
+    System.out.println(e + "\nYou can't divide an integer by 0");
+    }
+    System.out.println("Pick a new number to divide by.");
+    double newNum = s.nextDouble();
+    System.out.println(favNum / newNum);
+    System.out.println("\nCool beans dude! \nOh, I never caught your name. What's your name?");
+    s = new Scanner(System.in);
+    String name = s.nextLine();
+    System.out.println("Hi, " + name + "!");
+    s.close();
+    System.out.println(name + ", how old are you?");
+    s = new Scanner(System.in);
+    int age = s.nextInt();
+    age++;
+    System.out.println("Next year you're going to turn " + age + "!\nWow that's old!");
+    age-=1;
+    System.out.println("What's the biggest number you can think of? (don't go over 2 billion)");
+    int bignum = s.nextInt();
+    System.out.println("Wow, " + bignum + ", that's a big number!");
+    int maxnum = Integer.MAX_VALUE;
+    int difrence = Integer.MAX_VALUE - bignum;
+    System.out.println(difrence);
+    System.out.println("We can add " + difrence + " until we hit our max.");
+    bignum =+ difrence;
+    System.out.println("Our max number is " + bignum + "\nNow let's add a tiny bit and see what happens");
+    s = new Scanner(System.in);
+    Double add = s.nextDouble();
+    bignum += add;
+    System.out.println(bignum);
+  }
 }
